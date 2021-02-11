f8217173f0c25ea37442e260bb30e37f4ae92ead
Janith96 <janith0000@gmail.com>
2021-02-11 21:11:22 +0530

    testing db

--- Supporters Data System/suggestions.vb
+++ Supporters Data System/suggestions.vb
@@ -1,4 +1,5 @@
-﻿Public Class suggestions
+﻿Imports System.Data.SQLite
+Public Class suggestions
     Private Sub resetbtn_Click(sender As Object, e As EventArgs) Handles resetbtn.Click
         Dim txt As Control
         For Each txt In Me.Controls 'panel.controls if in a group
@@ -18,4 +19,23 @@
     Private Sub Exitbtn_Click(sender As Object, e As EventArgs) Handles Exitbtn.Click
         Application.Exit()
     End Sub
+
+    Private Sub suggestions_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'getting database
+        Dim conn As New SQLiteConnection("Data Source = C:\Users\janit\Desktop\Suggestions.db")
+        conn.Open()
+
+        Dim sqlcmd As New SQLiteCommand
+        sqlcmd.Connection = conn
+        sqlcmd.CommandText = "Select * from Suggestions"
+
+        Dim sqlread As SQLiteDataReader = sqlcmd.ExecuteReader
+        Dim sqldt As New DataTable
+        sqldt.Load(sqlread)
+        sqlread.Close()
+        conn.Close()
+
+        DataGridView1.DataSource = sqldt
+    End Sub
+
 End Class
